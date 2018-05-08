5334c6aa74134aee926b0f2e6c4494e444609863
victorjaquez <victorajaquez@gmail.com>
2018-05-07 19:52:04 -0700

    dashboard, and profile fields

---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+
+class CreateProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      amount: "",
+      errors: {}
+    };
+  }
+
+  render() {
+    return (
+      <div className="create-profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Create your profile</h1>
+              <p className="lead text-center">
+                Lets get some information to make your profile stand out.
+              </p>
+              <small className="d-block pb-3">* = required fields</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+CreateProfile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps)(CreateProfile);
