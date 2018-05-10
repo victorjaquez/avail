761b396c60486eb43be9bb1c2692b51783b0d5a8
victorjaquez <victorajaquez@gmail.com>
2018-05-09 21:06:20 -0700

    fixed heroku deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 app.use(express.json());
 app.use(express.static("client"));
@@ -21,14 +22,30 @@ app.use("/user", usersController);
 const profileController = require("./controllers/profile.js");
 app.use("/profile", profileController);
 
-const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/avail";
-mongoose.connect(mongoURI);
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-mongoose.connection.once("open", () => {
-  console.log("connected to mongo . . .");
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/avail";
+// mongoose.connect(mongoURI);
+
+// mongoose.connection.once("open", () => {
+//   console.log("connected to mongo . . .");
+// });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log("listening on port 5000. . .");
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
